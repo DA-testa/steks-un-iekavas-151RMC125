ececbf4ba0b1242812e6b3fe97b053a104efe4ee
151RMC125 <Konstantins.Sinica@edu.rtu.lv>
2023-02-20 02:53:21 +0200

    Update v1.01

--- main.py
+++ main.py
@@ -22,9 +22,10 @@ def find_mismatch(text):
                 return i+1
             opening_brackets_stack.pop()
 
-    if opening_brackets_stack:
+    if not opening_brackets_stack:
+        return "Success"
+    else:
         return opening_brackets_stack[0].position
-    return "Success"
 
 
 def main():
